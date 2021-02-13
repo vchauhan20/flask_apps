dda0143e470b494f77e641b3355d28194698ba63
LAPTOP-FGPTHUNM\Karan <vijay.chauhan@plutoshift.com>
2021-02-13 15:49:47 -0500

    'commented'

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def home():
 def second():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
