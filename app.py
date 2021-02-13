5940f9664fffcf477d6597d41348ed3dd6a555ce
LAPTOP-FGPTHUNM\Karan <vijay.chauhan@plutoshift.com>
2021-02-13 18:03:01 -0500

    'Proc file commented'

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def second():
+    text = [x for x in request.form.values()]
+    print(text[0])
     return render_template('chart.html',toxic=100,hate=50,threat=20,Neutral=0)
 
 if __name__ == '__main__':
