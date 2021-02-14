ec82ad9eb04ba2657b4574c2db3611048069a4e7
LAPTOP-FGPTHUNM\Karan <vijay.chauhan@plutoshift.com>
2021-02-14 16:01:00 -0500

    'sentiment analysis of text'

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask,request,render_template
-from model import create_model
+from model import create_model,preprocess_data
+from tensorflow.keras.preprocessing.text import Tokenizer
+from joblib import load
 
 app = Flask(__name__)
+new_model = create_model()
+tokenizer = load('tokenizer.pkl')
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",positive =0 , negative =0)
 
 @app.route('/predict',methods=['POST'])
 def second():
     text = [x for x in request.form.values()]
-    print(text[0])
-    return render_template('chart.html',toxic=100,hate=50,threat=20,Neutral=0)
+    processed_data = preprocess_data(text,tokenizer)
+
+    results = new_model.predict(processed_data)
+    print(results)
+    if results > .75:
+        positive = results
+        negative = 1 - positive
+    else:
+        positive = results
+        negative = 1 - positive
+    return render_template('index.html',positive = positive, negative = negative)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,5 +1,14 @@
 from tensorflow.keras.models import load_model
-def create_model():
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-    model = load_model(saved_model)
+def create_model():
+    model = load_model('model.hdf5')
     return model
+
+def preprocess_data(input_str,tokenizer):
+
+    tokens = tokenizer.texts_to_sequences(input_str)
+    padded = pad_sequences(tokens, maxlen=100)
+    return padded
+
