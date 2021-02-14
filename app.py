7e5d3156bdd255549ef9cc736715a02a5611f24e
LAPTOP-FGPTHUNM\Karan <vijay.chauhan@plutoshift.com>
2021-02-13 20:20:12 -0500

    'Proc file commented'

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request,render_template
-
+import model.py
 
 app = Flask(__name__)
 
---
+++ templates/model.py
@@ -0,0 +1,5 @@
+from tensorflow.keras.models import load_model
+def create_model():
+
+    model = load_model(saved_model)
+    return model
